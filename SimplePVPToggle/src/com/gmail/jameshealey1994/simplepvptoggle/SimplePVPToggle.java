ffed2c5bbd55efebae977abda98e02391419029b
m2019524 <m2019524@linux37.scm.tees.ac.uk>
2013-10-18 10:37:39 +0100

    Package edited and SimplePVPToggle class added.

---
+++ SimplePVPToggle/src/com/gmail/jameshealey1994/simplepvptoggle/SimplePVPToggle.java
@@ -0,0 +1,9 @@
+package com.gmail.jameshealey1994.simplepvptoggle;
+
+/**
+ * Plugin to simply and easily set the PVP status of players, worlds, or the entire server.
+ * @author m2019524
+ */
+public class SimplePVPToggle {
+
+}
