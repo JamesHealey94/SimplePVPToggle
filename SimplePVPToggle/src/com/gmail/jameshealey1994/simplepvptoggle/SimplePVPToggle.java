b012809a0b277cf73ed9ac836fd360d5305e0899
m2019524 <m2019524@linux37.scm.tees.ac.uk>
2013-10-18 10:40:31 +0100

    Bukkit library added and SimplePVPPlugin class updated.

--- SimplePVPToggle/src/com/gmail/jameshealey1994/simplepvptoggle/SimplePVPToggle.java
+++ SimplePVPToggle/src/com/gmail/jameshealey1994/simplepvptoggle/SimplePVPToggle.java
@@ -1,9 +1,11 @@
 package com.gmail.jameshealey1994.simplepvptoggle;
 
+import org.bukkit.plugin.java.JavaPlugin;
+
 /**
  * Plugin to simply and easily set the PVP status of players, worlds, or the entire server.
  * @author m2019524
  */
-public class SimplePVPToggle {
-
+public class SimplePVPToggle extends JavaPlugin {
+    //
 }
