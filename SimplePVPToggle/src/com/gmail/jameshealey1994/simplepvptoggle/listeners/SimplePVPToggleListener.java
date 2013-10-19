19dc07d7e1c7764363bbd70c45a624bcd80f6559
James Healey <jameshealey1994@gmail.com>
2013-10-19 05:09:26 +0100

    Progress on Listener class, almost working.

--- SimplePVPToggle/src/com/gmail/jameshealey1994/simplepvptoggle/listeners/SimplePVPToggleListener.java
+++ SimplePVPToggle/src/com/gmail/jameshealey1994/simplepvptoggle/listeners/SimplePVPToggleListener.java
@@ -1,13 +1,11 @@
 package com.gmail.jameshealey1994.simplepvptoggle.listeners;
 
 import org.bukkit.configuration.file.FileConfiguration;
-import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
-import org.bukkit.event.entity.EntityDamageEvent;
-import org.bukkit.event.entity.EntityDamageEvent.DamageCause;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
 
 /**
  * Listener class for the SimplePVPToggle plugin.
@@ -15,44 +13,54 @@ import org.bukkit.event.entity.EntityDamageEvent.DamageCause;
  */
 public class SimplePVPToggleListener implements Listener {
 
+    /**
+     * Configuration file used to get PvP value.
+     */
     FileConfiguration config;
     
+    /**
+     * Constructor used to set config.
+     * @param config Config used to retrieve PvP values from
+     */
     public SimplePVPToggleListener(FileConfiguration config) {
         this.config = config;
     }
     
     /**
-     * Temporary event to be changed later.
-     * Probably unused and to be removed. Just here as an example for later.
+     * Entity damaged by another entity event.
+     * Cancels the event and sends damager a message if either damager or attacked player have not enabled PvP
      * @param event The event being handled
      */
     @EventHandler (priority = EventPriority.LOWEST)
-    public void onDamage(EntityDamageEvent event) {
-        if (event.getCause().equals(DamageCause.ENTITY_ATTACK)) {
-            if (event.getEntityType().equals(EntityType.PLAYER)) {
-                final Player attackedPlayer = (Player) event.getEntity();
-                if (canPVP(attackedPlayer)) {
-                    // check the attacked and the damaged can both PVP, if not, cancel.
+    public void onDamage(EntityDamageByEntityEvent event) {
+        if (event.getEntity() instanceof Player) {
+            if (event.getDamager() instanceof Player) {
+                final Player attacker = (Player) event.getDamager();
+                if (canPVP(attacker)) {
+                    final Player attackedPlayer = (Player) event.getEntity();
+                    if (canPVP(attackedPlayer)) {
+                        // PVP is allowed. No changes needed.
+                    } else {
+                        attacker.sendMessage("Attack Cancelled - " + attackedPlayer.getDisplayName() + " does not have PvP enabled.");
+                    }
+                } else {
+                    attacker.sendMessage("Attack Cancelled - You do not have PvP enabled.");
                 }
             }
         }
     }
     
     /**
-     * 
-     * @param player
-     * @return 
+     * Helper method used to check if a player's PvP value is set to true in the config.
+     * It first looks for a specific value for that player in that world.
+     * If that is not found, it is set to the default value for the world the player is currently in.
+     * If that is not found, it is set to the default value for the server.
+     * If there is an error with the default value for the server, it is set to false by default.
+     * @param player The player being checked
+     * @return If the player can PVP in that world
      */
-    public boolean canPVP(Player player) {      
-        // check player
-        if (config.getBoolean("ServerDefault", false)) {
-            
-        }
-        
-        // player.getWorld() check default value for server from config
-        
-        // check world the player is in
-        
-        return false;
+    public boolean canPVP(Player player) {
+        // TODO: use correct strings
+        return (config.getBoolean("Server.WorldName.PlayerName", config.getBoolean("ServerDefault.WorldName", config.getBoolean("ServerDefault", false))));
     }
 }
