4d1ce0281632d113f255233c9d0f4f12c0ae2700
James Healey <jameshealey1994@gmail.com>
2013-10-20 22:20:30 +0100

    Removed duplicate code (Reload command is now in separate class).

--- SimplePVPToggle/src/com/gmail/jameshealey1994/simplepvptoggle/SimplePVPToggle.java
+++ SimplePVPToggle/src/com/gmail/jameshealey1994/simplepvptoggle/SimplePVPToggle.java
@@ -5,8 +5,6 @@ import com.gmail.jameshealey1994.simplepvptoggle.commands.SimplePVPToggleCommand
 import com.gmail.jameshealey1994.simplepvptoggle.commands.SimplePVPToggleCommandExecutor;
 import com.gmail.jameshealey1994.simplepvptoggle.listeners.SimplePVPToggleListener;
 import java.util.List;
-import org.bukkit.ChatColor;
-import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
 
 /**
@@ -32,22 +30,6 @@ public class SimplePVPToggle extends JavaPlugin {
         // Set command executors
         // This will throw a NullPointerException if you don't have the command defined in your plugin.yml file!
         getCommand("spt").setExecutor(new SimplePVPToggleCommandExecutor(this, new HelpCommand()));
-    }
-
-    /**
-     * Reloads values from configuration file.
-     * @param sender The sender of the command
-     * @return If reload was completed correctly.
-     */
-    public boolean reload(CommandSender sender) {
-        if (sender.hasPermission("spt.reload")) {
-            reloadConfig();
-            sender.sendMessage(ChatColor.LIGHT_PURPLE + "Configuration reloaded.");
-            return true;
-        } else {
-            sender.sendMessage(ChatColor.RED + "You need permission 'spt.reload' to use this command.");
-            return false;
-        }
     }
 
     /**
