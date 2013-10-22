072b155c9c588f5309be18734a9239b16ac94a90
James Healey <jameshealey1994@gmail.com>
2013-10-22 22:05:08 +0100

    Started localisation work.

---
+++ SimplePVPToggle/src/com/gmail/jameshealey1994/simplepvptoggle/Localisation.java
@@ -0,0 +1,9 @@
+package com.gmail.jameshealey1994.simplepvptoggle;
+
+/**
+ * Class to help with localisation and custom messages.
+ * @author JamesHealey94 <jameshealey1994.gmail.com>
+ */
+public class Localisation {
+    // TODO
+}
