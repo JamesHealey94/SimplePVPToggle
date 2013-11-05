a29a084afdf4ed6aa16a954d81d065b8464bfa40
James Healey <jameshealey1994@gmail.com>
2013-11-05 23:46:16 +0000

    Remove unnecessary comment.

--- SimplePVPToggle/src/com/gmail/jameshealey1994/simplepvptoggle/commands/HelpCommand.java
+++ SimplePVPToggle/src/com/gmail/jameshealey1994/simplepvptoggle/commands/HelpCommand.java
@@ -8,9 +8,9 @@ import org.bukkit.command.CommandSender;
 
 /**
  * Class representing a SimplePVPToggle help command.
- * 
+ *
  * /pvp [help]      Shows help menu
- * 
+ *
  * @author JamesHealey94 <jameshealey1994.gmail.com>
  */
 public class HelpCommand extends SimplePVPToggleCommand {
@@ -19,7 +19,6 @@ public class HelpCommand extends SimplePVPToggleCommand {
      * Constructor to add aliases and permissions.
      */
     public HelpCommand() {
-        // TODO Make aliases configurable?
         this.aliases.add("help");
         this.aliases.add("h");
     }
@@ -30,8 +29,9 @@ public class HelpCommand extends SimplePVPToggleCommand {
         /*
          * Command can be used by anyone.
          */
-        sender.sendMessage(localisation.get(LocalisationEntry.HELP_HEADER, new Object[] {plugin.getDescription().getFullName()}));
-        
+        sender.sendMessage(localisation.get(LocalisationEntry.HELP_HEADER,
+                new Object[] {plugin.getDescription().getFullName()}));
+
         for (SimplePVPToggleCommand command : plugin.getCommands()) {
             if (CommandUtils.canExecute(command, sender, true)) {
                 sender.sendMessage(localisation.get(LocalisationEntry.HELP_ENTRY, new Object[] {command.aliases.get(0), command.getDescription(localisation)}));
