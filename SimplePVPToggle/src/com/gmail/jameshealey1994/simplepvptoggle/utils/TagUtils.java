e0b47ea529608086794385edbeada6bc84e9c634
James Healey <jameshealey1994@gmail.com>
2013-11-11 01:45:58 +0000

    Changed Tag refresh to only refresh nearby players that are visible (and not all player's in that world) to improve security against hacked clients.

--- SimplePVPToggle/src/com/gmail/jameshealey1994/simplepvptoggle/utils/TagUtils.java
+++ SimplePVPToggle/src/com/gmail/jameshealey1994/simplepvptoggle/utils/TagUtils.java
@@ -1,7 +1,9 @@
 package com.gmail.jameshealey1994.simplepvptoggle.utils;
 
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.kitteh.tag.TagAPI;
@@ -18,6 +20,11 @@ public abstract class TagUtils {
      */
     public static final String NAME = "TagAPI";
 
+    /**
+     * The width of a Minecraft chunk.
+     */
+    public static final int CHUNK_WIDTH = 16;
+    
     /**
      * Returns if TagAPI is enabled.
      *
@@ -70,12 +77,30 @@ public abstract class TagUtils {
      * Reloads all the player tags on the server.
      * Useful if the tags have changed in the configuration
      *
+     * Only refreshes nearby players that are visible (and not all player's in
+     * that world) to improve security against hacked clients.
+     * 
      * @param plugin    plugin with possible TagAPI
      */
     public static void reload(JavaPlugin plugin) {
         if (isEnabled(plugin)) {
+            final int maxPlayerViewDistance = plugin.getServer().getViewDistance() * CHUNK_WIDTH;
+            
             for (Player player : plugin.getServer().getOnlinePlayers()) {
-                TagAPI.refreshPlayer(player, new HashSet(player.getWorld().getPlayers()));
+                final int maxWorldHeight = player.getWorld().getMaxHeight();
+                final Set<Player> tagsToUpdate = new HashSet();
+                
+                final List<Entity> nearbyEntities = player.getNearbyEntities(maxPlayerViewDistance, maxWorldHeight, maxPlayerViewDistance);
+                for (Entity e : nearbyEntities) {
+                    if (e instanceof Player) {
+                        final Player p = (Player) e;
+                        if (player.canSee(p)) {
+                            tagsToUpdate.add(p);
+                        }
+                    }
+                }
+                
+                TagAPI.refreshPlayer(player, tagsToUpdate);
             }
         }
     }
